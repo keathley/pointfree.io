e89ebddce2a956d94454d6fecec7169107b6a12b
Chris Keathley <c@keathley.io>
2015-03-28 01:19:27 -0400

    Use generics instead of a custom toJSON method

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Web.Scotty as S
+import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
 import Network.Wai.Middleware.Static
 import System.Environment
@@ -8,12 +9,23 @@ import System.Process
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Monoid (mconcat)
-import Data.Aeson
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
 
 import qualified Data.Text.Lazy as T
 
+import GHC.Generics (Generic)
+
+data PFCode = PFCode { code :: String } deriving (Show, Generic)
+
+instance ToJSON PFCode
+
+pointsFree code = readProcess process (sanitizeArgs code) ""
+sanitizeArgs = words
+process = ".cabal-sandbox/bin/pointfree"
+
 main = do
   port <- liftM read $ getEnv "PORT"
+
   scotty port $ do
     middleware logStdoutDev
     middleware $ staticPolicy (noDots >-> addBase "static")
@@ -23,16 +35,7 @@ main = do
     post "/snippet" $ do
       code  <- param "code"
       pf    <- liftIO $ pointsFree code
-      S.json (PFCode pf)
+      json (PFCode pf)
 
     notFound $ do
       text "that route does not exist"
-
-pointsFree code = readProcess process (sanitizeArgs code) ""
-sanitizeArgs = words
-process = ".cabal-sandbox/bin/pointfree"
-
-data PFCode = PFCode { code :: String } deriving (Show)
-
-instance ToJSON PFCode where
-  toJSON (PFCode code) = object ["pointfree" .= code]
