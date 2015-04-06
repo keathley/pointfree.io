e506f6004fac77e0bdf9bb45d29f4231e09d122f
Chris Keathley <c@keathley.io>
2015-04-06 09:38:25 -0400

    Extract css as seperate file

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: "./frontend/entry.js",
   output: {
     path: __dirname + '/static',
     filename: 'bundle.js'
   },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].js"
+  },
   module: {
     loaders: [
       {
         test: /\.scss$/,
-        loaders: [
+        loader: ExtractTextPlugin.extract(
           'style-loader',
-          'css-loader',
-          'autoprefixer-loader',
-          'sass-loader'
-        ]
+          'css-loader!autoprefixer-loader!sass-loader'
+        )
       },
       { test: /\.css$/, loader: 'style-loader!css-loader' }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
 };
